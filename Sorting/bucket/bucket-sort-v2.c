662065a8c56a7f9e93067547822d091aac42da77
saiimonn <gementizasgg08@gmail.com>
2026-04-13 23:32:14 +0800

    feat: add smthng

---
+++ Sorting/bucket/bucket-sort-v2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} *Node;
+
+typedef struct {
+    Node p;
+    Node l;
+} Buckets;
+
+int main(){
+
+}
